c7c455ba15e480fd43b96492e56e7c9143e4e746
unknown <vickodhis.vn@gmail.com>
2022-09-07 16:18:53 +0300

    main

--- main.h
+++ main.h
@@ -21,7 +21,7 @@ extern char **environ;
 int prompt(void);
 char *_read(void);
 char *_fullpathbuffer(char **av, char *PATH, char *copy);
-int checkbuiltins(char **av, char *PATH, char *copy);
+int checkbuiltins(char **av, char *buffer, int exitstatus);
 int _forkprocess(char **av, char *buffer, char *fullpathbuffer);
 
 /* String Helper Functions */
@@ -39,12 +39,12 @@ int _splitPATH(char *str);
 int _PATHstrcmp(const char *s1, const char *s2);
 char *_concat(char *tmp, char **av, char *tok);
 
-/*Other Helper Func*/
+/*Other Helper Funcs*/
 
-char *_getenv(const(const char *name);
+char *_getenv(const char *name);
 int _env(void);
 void _puts(char *str);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 
-#endif /* MAIN_H */
+#endif /* MAIN_H/
